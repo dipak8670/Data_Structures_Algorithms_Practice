3b18c14de8eee5f2ffd07b2a5b74af91398e2159
Dipak <60582789+dipak8670@users.noreply.github.com>
2023-01-30 01:23:17 +0530

    Binary Search Tree Completes

--- BinaryTree/Tree.java
+++ BinaryTree/Tree.java
@@ -1,5 +1,10 @@
 package BinaryTree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Tree {
     private class Node{
         private int value;
@@ -76,6 +81,20 @@ public class Tree {
         postOrder(node.right);
         System.out.print(node.value+" ");
     }
+    public void BFS(){
+        BFS(root);
+    }
+    private void BFS(Node node) {
+        Queue<Node> q=new LinkedList<>();
+        q.add(node);
+        while(!q.isEmpty()){
+            Node n=q.remove();
+            System.out.print(n.value+" ");
+            if(n.left !=null)q.add(n.left);
+            if(n.right!=null)q.add(n.right);
+        }
+        System.out.println();
+    }
     public int findHeight(){
         return findHeight(root);
     }
@@ -95,5 +114,48 @@ public class Tree {
     }
     private boolean isLeafNode(Node node) {
         return node.left==null && node.right==null;
+    }
+    public int minValue(){
+        return minValue(root);
+    }
+    private int minValue(Node node){
+        if(node==null)return Integer.MAX_VALUE;
+        if(isLeafNode(node))return node.value;
+        return minValue(node.left);
+    }
+    public boolean isEqual(Tree other){
+        return isEqual(root,other.root);
+    }
+    private boolean isEqual(Node node1, Node node2) {
+        if(node1==null && node2==null)return true;
+        if(node1==null || node2==null)return false;
+        return node1.value == node2.value && isEqual(node1.left, node2.left) && isEqual(node1.right, node2.right);
+    }
+    public boolean isValidBinarySearchTree(){
+        return isValidBinarySearchTree(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    private boolean isValidBinarySearchTree(Node node, int minValue, int maxValue) {
+        if(node==null)return true;
+        if(node.value<minValue || node.value>maxValue)return false;
+        return isValidBinarySearchTree(node.left,minValue,node.value-1) && isValidBinarySearchTree(node.right,node.value+1,maxValue);
+    }
+    public void swap(){
+        Node node=root.left;
+        root.left=root.right;
+        root.right=node;
+    }
+    public List<Integer> printNodeAtK(int K){
+        List<Integer> result=new ArrayList<>();
+        printNodeAtK(root,K,result);
+        return result;
+    }
+    private void printNodeAtK(Node root2, int k, List<Integer> result) {
+        if(root2==null)return;
+        if(k==0){
+            result.add(root2.value);
+            return;
+        }
+        printNodeAtK(root2.left, k-1,result);
+        printNodeAtK(root2.right, k-1,result);
     }
 }
--- BinaryTree/TreeImp.java
+++ BinaryTree/TreeImp.java
@@ -7,12 +7,27 @@ public class TreeImp {
         t.add(7);
         t.add(15);
         t.add(4);
+        t.add(21);
+        t.add(1);
+        t.add(32);
+        Tree t2=new Tree();
+        t2.add(10);
+        t2.add(7);
+        t2.add(15);
+        t2.add(4);
         System.out.println(t.find(12));
         t.preorder();
         t.inOrder();
         t.postOrder();
+        t.BFS();
         System.out.println(t.findHeight());
         System.out.println(t.min());
+        System.out.println(t.minValue());
+        System.out.println(t.isEqual(t2));
+        System.out.println(t.isValidBinarySearchTree());
+        t2.swap();
+        System.out.println(t2.isValidBinarySearchTree());
+        System.out.println(t.printNodeAtK(2));
         System.out.println();
     }
     
